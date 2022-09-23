c3fd0b66e843f39464e4c569d9e2a4994ecf028f
guneykilicel <kilicelguney@gmail.com>
2022-09-23 10:40:24 +0300

    tasarım tamamlandı

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,16 @@ import React from "react";
 
 const Message = () => {
     return(
-        <div>Message</div>
+        <div className="message owner">
+            <div className="messageInfo">
+                <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300" alt="" />
+                <span>Just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300" alt="" />
+            </div>
+        </div>
     )
 }
 
