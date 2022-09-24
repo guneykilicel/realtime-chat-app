54628293cac1631e25f875fa85beaeba35193b0e
guneykilicel <kilicelguney@gmail.com>
2022-09-24 12:49:46 +0300

    search component tamamlandı

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Chats = () => {
+
+    const [chats, setChats] = useState([])
     return(
         <div className='chats'>
             <div className="userChat">
